d30655b9c534be30860ed43b334a1e41afa510a2
Harambe <mmarangelli@cps.edu>
2017-03-24 13:14:59 +0000

    Three page app

---
+++ projects/3page/js/appbackup.js
@@ -0,0 +1,46 @@
+/*global res*/
+/*global $*/
+/*global gapi*/
+
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+//NAILED IT
+
+$(function() {
+    $("form").on("submit", function(e) {
+       e.preventDefault();
+       
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 3,
+            order: "viewCount",
+            
+       }); 
+       
+       request.execute(function(response) {
+          var results = response.result;
+          $("#results").html("");
+          $.each(results.items, function(index, item) {
+            $.get("tpl/item.html", function(data) {
+                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            });
+          });
+          resetVideoHeight();
+       });
+    });
+    
+    $(window).on("resize", resetVideoHeight);
+});
+
+function resetVideoHeight() {
+    $(".video").css("height", $("#results").width() * 9/16);
+}
+
+//IGNORE THE ERROR BELOW THIS
+function init() {
+    gapi.client.setApiKey("AIzaSyBCewPmix-R_Cv3RTysFVYXoHZmCnjTfOc");
+    gapi.client.load("youtube", "v3", function() {
+        
+    });
+}
